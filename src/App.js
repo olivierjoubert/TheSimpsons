be9466d0b68bfa51fac07ac4a72fe62c1ea7cfc1
Olivier Joubert <olivier.joubert@gmail.com>
2021-09-16 14:51:54 +0200

    Integrating fetch for first display

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ const client = axios.create({
     baseURL: 'https://simpsons-quotes-api.herokuapp.com/quotes'
 })
 
-
-
 export default function App() {
   const [selectedQuote, setQuote] = React.useState('');
-  
-  const getQuote = () => {
-    client
-        .get('')
-        .then((res) => res.data)
-        .then((data) => setQuote(data[0]));
+  function getNewQuote () {
+      client
+          .get("")
+          .then((res) => res.data)
+          .then((data) => setQuote(data[0]));
   }
+
+  React.useEffect(() => {
+            getNewQuote();
+        }, []);
 
   return (
     <div className="App">
-      <button onClick={getQuote}>Click for another quote</button>
+      <button onClick={getNewQuote}>Click for another quote</button>
       <DisplayQuote selectedQuote={selectedQuote}/>
     </div>
   );
